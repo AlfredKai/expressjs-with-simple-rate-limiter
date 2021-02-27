c5727a6538519aebe562df2d0923447b21a14f1b
Kai Chang <alfred.kai.chang@gmail.com>
2021-02-27 15:43:51 +0800

    [feature] add requestCounter.js

---
+++ middleware/rate-limiter/requestCounter.js
@@ -0,0 +1,30 @@
+const redis = require('redis');
+
+class RequestCounter {
+  constructor(window) {
+    this.window = window;
+    this.redisClient = redis.createClient();
+    this.redisClient.on('error', function (error) {
+      console.error(error);
+    });
+  }
+
+  add(key) {
+    const self = this;
+    return new Promise((resolve, reject) => {
+      self.redisClient
+        .multi()
+        .incr(key)
+        .ttl(key)
+        .exec(function (err, replies) {
+          if (err) {
+            return reject(err);
+          }
+          if (replies[1] == -1) {
+            self.redisClient.expire(key, self.window);
+          }
+          resolve(replies[0], replies[1]);
+        });
+    });
+  }
+}
